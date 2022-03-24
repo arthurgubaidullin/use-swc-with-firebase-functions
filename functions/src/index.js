3c6dfe9ad77e704db4caf5c3f3eedf6f876110c8
Arthur G <lapteuh@gmail.com>
2022-03-24 18:26:55 +0500

    Refactor code to use import/export

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,9 @@
-const functions = require("firebase-functions");
+import {https, logger} from "firebase-functions";
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+export const helloWorld = https.onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
