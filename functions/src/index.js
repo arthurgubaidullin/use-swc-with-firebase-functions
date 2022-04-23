dbdcfd84e9ad8020dffd690f1d7226cfafbc89b4
Arthur G <lapteuh@gmail.com>
2022-04-24 03:44:55 +0500

    Enable source maps

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,4 +1,5 @@
 import {https, logger} from "firebase-functions";
+import "source-map-support/register";
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
