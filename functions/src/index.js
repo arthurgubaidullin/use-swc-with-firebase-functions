bdaf51954ef3e919b500478fdd5a4d8076724c63
Arthur G <lapteuh@gmail.com>
2022-06-11 19:01:30 +0500

    Update example of code

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,10 +1,78 @@
-import {https, logger} from "firebase-functions";
+// @ts-check
+
 import "source-map-support/register";
+import {https, logger, Response} from "firebase-functions";
+import {initializeApp} from "firebase-admin";
+import {FieldValue, getFirestore} from "firebase-admin/firestore";
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+/**
+ * @typedef {{
+ *   amount: number;
+ *   currency: "USD" | "RUB";
+ * }} Money
+ */
 
-export const helloWorld = https.onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true, computed: 50 + 50});
-  response.send("Hello from Firebase!");
-});
+/**
+ * @typedef {{
+ *   name: string;
+ *   price: Money;
+ * }} NewProduct
+ */
+
+initializeApp();
+
+export const addProduct = https.onRequest(addProductHandler);
+
+/**
+ * @param {https.Request} request
+ * @param {Response} response
+ * @returns {Promise<void>}
+ */
+async function addProductHandler(request, response) {
+  const newProduct = /** @type {NewProduct} */ (request.body);
+  logger.info("Adding product.", {product: newProduct});
+  const productId = await _addProduct(newProduct);
+  logger.info("1 + 2 =", add(1, 2));
+  response.json({productId}).end();
+}
+
+/**
+ * @param {NewProduct} newProduct
+ * @returns {Promise<string>}
+ */
+async function _addProduct(newProduct) {
+  const ref = await getFirestore()
+    .collection("products")
+    .add(
+      addCreatedAtField({
+        ...newProduct,
+      })
+    );
+  return ref.id;
+}
+
+/**
+ * @template {{
+ *   [k: string]: unknown;
+ * }} T
+ * @param {T} something
+ * @returns {T & {
+ *   createdAt: FieldValue;
+ * }}
+ */
+function addCreatedAtField(something) {
+  return {
+    ...something,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+}
+
+/**
+ * @deprecated
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+  return a + b;
+}
